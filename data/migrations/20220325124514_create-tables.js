011b32d3121bafca8734cb43a5af945ec6073bac
XavierTwitty <x.twitty@gmail.com>
2022-03-25 09:15:08 -0400

    wip

--- data/migrations/20220325124514_create-tables.js
+++ data/migrations/20220325124514_create-tables.js
@@ -1,31 +1,30 @@
 
 exports.up = async function(knex) {
   await knex.schema 
-
-.createTable('projects', table => {
+    .createTable('projects', table => {
     table.increments('project_id')
+ 
+
 })
 .createTable('resources', table => {
     table.increments('resource_id')
+    
 })
 .createTable('tasks', table => {
     table.increments('task_id')
-})
-.createTable('projects_resources', table => {
-    table.increments('projects_resource_id')
-})  
-.createTable('projects', table => {
-    table.increments('project_id')
+  
 })
 
+.createTable('project_resources', table => {
+    table.increments('project_resource_id')
+})
 
-};
+}
 
 exports.down = async function(knex) {
-  await knex.schema 
-
-.dropTablesExists('project_resources')
-.dropTablesExists('tasks')
-.dropTablesExists('resources')  
-.dropTablesExists('projects')
+await knex.schema 
+.dropTableIfExists("project_resources")
+.dropTableIfExists("tasks")
+.dropTableIfExists("resources")
+.dropTableIfExists("projects");
 };
